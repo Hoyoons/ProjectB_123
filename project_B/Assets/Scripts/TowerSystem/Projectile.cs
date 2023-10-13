1222ba0029d383a7707600f6b2fe8c9ee479a6a6
hoyoons <belunar74@g.shingu.ac.kr>
2023-10-13 10:30:28 +0900

    Tower System
    적 인식과 발사체 구현

---
+++ project_B/Assets/Scripts/TowerSystem/Projectile.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float moveSpeed;
+    public float damagedAmount;
+    public bool hasDamaged;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * moveSpeed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag =="Enemy" && !hasDamaged)
+        {
+            hasDamaged = true;
+        }
+
+        Destroy(gameObject);
+    }
+}
