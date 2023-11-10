e41428f4c9b9b890f613fc63e7bff1143c06fd3c
hoyoons <belunar74@g.shingu.ac.kr>
2023-11-10 09:30:30 +0900

    공허로 끝없이 날라가던 총알을 사라지게
    ㅈㄱㄴ

--- project_B/Assets/Scripts/TowerSystem/Projectile.cs
+++ project_B/Assets/Scripts/TowerSystem/Projectile.cs
@@ -25,4 +25,14 @@ public class Projectile : MonoBehaviour
 
         Destroy(gameObject);
     }
+    public string targetTag = "wall"; // 대상 콜라이더의 태그를 지정합니다.
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag(targetTag))
+        {
+            // 충돌한 콜라이더의 태그가 지정한 태그와 일치할 경우 실행되는 코드
+            Destroy(gameObject); // 총알을 파괴합니다.
+        }
+    }
 }
